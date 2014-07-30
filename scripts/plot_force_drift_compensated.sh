6fb7423f2a88d4d22ddd2e2275b68b1010029a42
fevb <fevb@kth.se>
2014-07-30 12:07:56 +0200

    added plot script of drift compensated FT signals

---
+++ scripts/plot_force_drift_compensated.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# plots drift-compensated force readings from F/T sensor
+
+# usage: plot_force.sh left
+#        plot_force.sh right
+
+# OR:    rosrun dumbo_scripts plot_force.sh left
+
+# parameter specifies left or right arm
+
+rqt_plot /$1_arm_ft_sensor/ft_drift_compensated/wrench/force/x /$1_arm_ft_sensor/ft_drift_compensated/wrench/force/y /$1_arm_ft_sensor/ft_drift_compensated/wrench/force/z
